61819746982f812f74e3daaf758ea8bf27c42429
Jakkrit <jakkrit.sonsanit@gmail.com>
2022-10-15 00:57:14 +0700

    Ugly Number

--- Ugly_numbers/main.py
+++ Ugly_numbers/main.py
@@ -1,20 +1,34 @@
-def isUgly(n):
-    for a in [2, 3, 5]:
-        while n % a == 0:
-            n = n / a
-    return 1 if n == 1 else 0
-        
-def main(n):
-    c = 0
-    ans = 1
-    while c <= n:
-        ans += 1
-        
-        if isUgly(ans):
-            c += 1
+def main(n: int):
+    ugly = [0] * n
 
-    print(ans)
-    
+    ugly[0] = 1
+
+    next_2 = 2
+    next_3 = 3
+    next_5 = 5
+
+    i2 = i3 = i5 = 0
+
+    for i in range(1, n):
+
+        ugly[i] = min(next_2, next_3, next_5)
+
+        if ugly[i] == next_2:
+            i2 += 1
+            next_2 = ugly[i2] * 2
+
+        if ugly[i] == next_3:
+            i3 += 1
+            next_3 = ugly[i3] * 3
+
+        if ugly[i] == next_5:
+            i5 += 1
+            next_5 = ugly[i5] * 5
+
+    return ugly[-1]
+
 
 if __name__ == "__main__":
-    main(int(input()))
+    n = int(input())
+    print(main(n))
+
